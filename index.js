1f08da5bdc34a48bf1cc12dcbf53a0f93d8c88b6
zhongchiyu <zhongchiyu@gmail.com>
2015-11-27 11:42:53 +0800

    Fix: INCREMENT only support one metric

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-'use strict';
+'use strict'
 
-var os = require('os');
-var http = require('http');
-var request = require('request');
-var debug = require('debug')('open-falcon:index');
+var os = require('os')
+var http = require('http')
+var request = require('request')
+var debug = require('debug')('open-falcon:index')
 
 function getTrimedHostname() {
-    var hostname = os.hostname();
+    var hostname = os.hostname()
     if (hostname.split('.').length) {
-        return hostname.split('.')[0];
+        return hostname.split('.')[0]
     }
-    return hostname;
+    return hostname
 }
 
 /**
- * var Falcon = require('open-falcon');
- * Falcon.init('http://localhost:6060', 'exampleProject');
+ * var Falcon = require('open-falcon')
+ * Falcon.init('http://localhost:6060', 'exampleProject')
  *
  * var falcon1 = new Falcon({
  *   tags: 'tagkey=tagvalue',
- * });
+ * })
  *
  * falcon1
  *   .step(60)
  *   .tag('anotherkey', 'anothervalue')
- *   .gauge('metricName', 100);
+ *   .gauge('metricName', 100)
  *
- * var falcon2 = new Falcon();
- * falcon2.counter('metricName2', 20);
+ * var falcon2 = new Falcon()
+ * falcon2.counter('metricName2', 20)
  *
- * var falcon3 = new Falcon();
+ * var falcon3 = new Falcon()
  * falcon3
  *   .step(20)
- *   .increment('incrementMetric');
+ *   .increment('incrementMetric1')
+ *   .increment('incrementMetric2')
  */
 
 /**
@@ -41,19 +42,19 @@ function getTrimedHostname() {
  *   tags: optional
  */
 function Falcon(options) {
-    debug('constructor', options);
-    options = options || {};
+    debug('constructor', options)
+    options = options || {}
 
-    this._lock = false;
-    this._queue = [];
-    this._increment = null;
+    this._lock = false
+    this._queue = []
+    this._increment = {}
 
     // step trigger autoFlush
-    this.step(options.step === undefined ? Falcon.DEFAULT_STEP : options.step);
-    this.tag('project', Falcon.PROJECT);
-    this._handler = options.handler || Falcon.DEFAULT_HANDLER;
+    this.step(options.step === undefined ? Falcon.DEFAULT_STEP : options.step)
+    this.tag('project', Falcon.PROJECT)
+    this._handler = options.handler || Falcon.DEFAULT_HANDLER
     if (options.tags) {
-        this.tag(options.tags);
+        this.tag(options.tags)
     }
 }
 
@@ -62,9 +63,9 @@ function Falcon(options) {
  * options
  *   project: project name, set as project=<project> tag
  */
-Falcon.DEFAULT_STEP = 60;
-Falcon.DEFAULT_COUNTER_TYPE = 'GAUGE';
-Falcon.ENDPOINT = getTrimedHostname();
+Falcon.DEFAULT_STEP = 60
+Falcon.DEFAULT_COUNTER_TYPE = 'GAUGE'
+Falcon.ENDPOINT = getTrimedHostname()
 
 /**
  * @param {string} api
@@ -72,92 +73,97 @@ Falcon.ENDPOINT = getTrimedHostname();
  * @param {Function} handler optional request handler
  */
 Falcon.init = function(api, project, handler) {
-    debug('init', api, project);
-    Falcon.API = api;
-    Falcon.PROJECT = project;
-    Falcon.DEFAULT_HANDLER = handler || function noop(){};
-    return Falcon;
-};
+    debug('init', api, project)
+    Falcon.API = api
+    Falcon.PROJECT = project
+    Falcon.DEFAULT_HANDLER = handler || function noop(){}
+    return Falcon
+}
+
+Falcon.prototype.destroy = function() {
+    debug('destroy')
+    clearTimeout(this._timeout)
+}
 
 Falcon.prototype.tag = function(key, value) {
-    debug('tag', key, value);
+    debug('tag', key, value)
     if (this._lock) {
-        this.emit('error', new Error('Falcon locked, can\'t alter tag'));
-        return this;
+        this.emit('error', new Error('Falcon locked, can\'t alter tag'))
+        return this
     }
 
     if (!this._tags) {
-        this._tags = '';
+        this._tags = ''
     } else {
-        this._tags = this._tags + ',';
+        this._tags = this._tags + ','
     }
     if (arguments.length === 2) {
-        this._tags = this._tags + key + '=' + value;
+        this._tags = this._tags + key + '=' + value
     } else {
-        this._tags = this._tags + key;
+        this._tags = this._tags + key
     }
-    return this;
-};
+    return this
+}
 
 Falcon.prototype.step = function(step) {
-    debug('step', step);
+    debug('step', step)
     if (this._lock) {
-        this.emit('error', new Error('Falcon locked, can\'t alter step'));
-        return this;
+        this.emit('error', new Error('Falcon locked, can\'t alter step'))
+        return this
     }
 
-    this._step = step;
+    this._step = step
     // autoFlush timeout should change everytime step changes
-    this.autoFlush();
-    return this;
-};
+    this.autoFlush()
+    return this
+}
 
 Falcon.prototype.gauge = function(metric, value, options) {
-    debug('gauge', metric, value, options);
-    options = options || {};
-    options.counterType = 'GAUGE';
-    this.push(metric, value, options);
-    return this;
-};
+    debug('gauge', metric, value, options)
+    options = options || {}
+    options.counterType = 'GAUGE'
+    this.push(metric, value, options)
+    return this
+}
 
 Falcon.prototype.counter = function(metric, value, options) {
-    debug('counter', metric, value, options);
-    options = options || {};
-    options.counterType = 'COUNTER';
-    this.push(metric, value, options);
-    return this;
-};
+    debug('counter', metric, value, options)
+    options = options || {}
+    options.counterType = 'COUNTER'
+    this.push(metric, value, options)
+    return this
+}
 
 Falcon.prototype.increment = function(metric, options) {
-    debug('increment', metric, options);
-    options = options || {};
-    options.counterType = 'INCREMENT';
-    this.push(metric, 1, options);
-    return this;
-};
+    debug('increment', metric, options)
+    options = options || {}
+    options.counterType = 'INCREMENT'
+    this.push(metric, 1, options)
+    return this
+}
 
 /**
  * @private
  */
 Falcon.prototype.now = function() {
-    return Math.floor(Date.now() / 1000);
-};
+    return Math.floor(Date.now() / 1000)
+}
 
 /**
  * @private
  */
 Falcon.prototype.createItem = function(metric, value, options) {
-    options.tags = options.tags ? (','+options.tags) : '';
+    options.tags = options.tags ? (','+options.tags) : ''
 
-    var endpoint = options.endpoint || Falcon.ENDPOINT;
-    var timestamp = this.now();
-    var step = options.step || this._step;
-    var counterType = options.counterType;
-    var tags = this._tags + options.tags;
+    var endpoint = options.endpoint || Falcon.ENDPOINT
+    var timestamp = this.now()
+    var step = options.step || this._step
+    var counterType = options.counterType
+    var tags = this._tags + options.tags
 
     if (counterType === 'INCREMENT') {
         // reset fake counterType
-        counterType = 'GAUGE';
+        counterType = 'GAUGE'
     }
 
     return {
@@ -168,82 +174,82 @@ Falcon.prototype.createItem = function(metric, value, options) {
         step: step,
         counterType: counterType,
         tags: tags,
-    };
-};
+    }
+}
 
 /**
  * @private
  */
 Falcon.prototype.push = function(metric, value, options) {
-    var counterType = options.counterType || Falcon.DEFAULT_COUNTER_TYPE;
+    var counterType = options.counterType || Falcon.DEFAULT_COUNTER_TYPE
     if (counterType === 'GAUGE' || counterType === 'COUNTER') {
-        this._queue.push(this.createItem(metric, value, options));
+        this._queue.push(this.createItem(metric, value, options))
     } else if (counterType === 'INCREMENT') {
-        if (!this._increment) {
-            this._increment = this.createItem(metric, value, options);
+        if (!this._increment[metric]) {
+            this._increment[metric] = this.createItem(metric, value, options)
         } else {
-            this._increment.value = this._increment.value + 1;
+            this._increment[metric].value = this._increment[metric].value + 1
         }
     }
-    this.checkAndFlush();
-    return this;
-};
+    this.checkAndFlush()
+    return this
+}
 
 /**
  * @private
  */
 Falcon.prototype.autoFlush = function() {
-    debug('autoFlush');
-    clearTimeout(this._timeout);
-    this.checkAndFlush();
+    debug('autoFlush')
+    clearTimeout(this._timeout)
+    this.checkAndFlush()
 
     // if step equals to zero, reset it to 1
-    var step = this._step || 1;
-    var self = this;
+    var step = this._step || 1
+    var self = this
     this._timeout = setTimeout(function() {
-        self.autoFlush();
-    }, step * 1000);
-};
+        self.autoFlush()
+    }, step * 1000)
+}
 
 /**
  * @private
  */
 Falcon.prototype.checkAndFlush = function() {
-    debug('checkAndFlush');
-    var timestamp = this.now();
-    if (this._increment) {
-        if (timestamp - this._increment.timestamp >= this._step) {
-            var increment = this._increment;
-            this._queue.push(increment);
+    debug('checkAndFlush')
+    var timestamp = this.now()
+    for (var metric in this._increment) {
+        var increment = this._increment[metric]
+        if (timestamp - increment.timestamp >= this._step) {
+            this._queue.push(increment)
             // reset increment rather than delete it
-            this._increment = this.createItem(increment.metric, 0, {counterType: 'INCREMENT'});
+            this._increment[metric] = this.createItem(increment.metric, 0, {counterType: 'INCREMENT'})
         }
     }
 
     if (this._queue.length) {
         if (timestamp - this._queue[0].timestamp >= this._step) {
-            this.flush();
+            this.flush()
         }
     }
-};
+}
 
 /**
  * @private
  */
 Falcon.prototype.flush = function() {
-    debug('flush', this._queue);
-    var queue = this._queue;
+    debug('flush', this._queue)
+    var queue = this._queue
     if (!queue.length) {
-        return;
+        return
     }
-    this._queue = [];
+    this._queue = []
 
-    var self = this;
+    var self = this
     request.post({
         url: Falcon.API,
         body: JSON.stringify(queue),
     }, this._handler)
-        .on('error', this._handler);
-};
+        .on('error', this._handler)
+}
 
-module.exports = Falcon;
+module.exports = Falcon
--- test/index.js
+++ test/index.js
@@ -1,128 +1,151 @@
-'use strict';
-var Falcon = require('..');
-var debug = require('debug')('open-falcon:test');
+'use strict'
+var Falcon = require('..')
+var debug = require('debug')('open-falcon:test')
+var async = require('async')
 
 describe('open-falcon', function() {
-    this.timeout(5000);
+    this.timeout(50000)
 
-    var api = 'http://localhost:6060';
-    var project = 'projectName';
-    Falcon.init(api, project);
+    var api = 'http://localhost:6060'
+    var project = 'projectName'
+    Falcon.init(api, project)
 
     it('should initialize api and project', function(done) {
         var falcon = new Falcon({
             step: 0,
-        });
+        })
 
         var metric =  'metricName'
-        var value = 100;
-        falcon.gauge(metric, value);
+        var value = 100
+        falcon.gauge(metric, value)
 
         getClientRequestBody(6060, function(queue) {
-            queue[0].tags.should.equal('project='+project);
-            queue[0].metric.should.equal(metric);
-            queue[0].value.should.equal(value);
-            done();
-        });
-    });
+            queue[0].tags.should.equal('project='+project)
+            queue[0].metric.should.equal(metric)
+            queue[0].value.should.equal(value)
+            done()
+        })
+    })
 
     it('should support GAUGE and COUNTER counterType', function(done) {
         var falcon = new Falcon({
             step: 0,
-        });
+        })
 
         var metric =  'metricName'
-        var value = 100;
-        falcon.gauge(metric, value);
+        var value = 100
+        falcon.gauge(metric, value)
 
         getClientRequestBody(6060, function(queue) {
-            queue[0].counterType.should.equal('GAUGE');
+            queue[0].counterType.should.equal('GAUGE')
 
-            falcon.counter(metric, value);
+            falcon.counter(metric, value)
             getClientRequestBody(6060, function(queue) {
-                queue[0].counterType.should.equal('COUNTER');
-                done();
-            });
-        });
-    });
+                queue[0].counterType.should.equal('COUNTER')
+                done()
+            })
+        })
+    })
 
     it('should support fake INCREMENT counterType', function(done) {
-        var falcon = new Falcon();
+        var falcon = new Falcon()
 
-        var metric = 'metricName';
+        var metric = 'metricName'
         falcon
             .step(1)
             .increment(metric)
-            .increment(metric);
+            .increment(metric)
 
         setTimeout(function() {
-            falcon.increment(metric);
-        }, 1000);
+            falcon.increment(metric)
+        }, 1000)
 
         getClientRequestBody(6060, function(queue) {
-            queue[0].counterType.should.equal('GAUGE');
-            queue[0].value.should.equal(2);
+            queue[0].counterType.should.equal('GAUGE')
+            queue[0].value.should.equal(2)
 
-            done();
-        });
-    });
+            falcon.destroy()
+            done()
+        })
+    })
+
+    it('should support multiple INCREMENT on single falcon instance', function(done) {
+        var falcon = new Falcon()
+
+        var metric1 = 'metricName1'
+        var metric2 = 'metricName2'
+        falcon
+            .step(1)
+            .increment(metric1)
+            .increment(metric2)
+            .increment(metric1)
+
+        getClientRequestBody(6060, function(queue) {
+            queue[0].counterType.should.equal('GAUGE')
+            queue[0].value.should.equal(2)
+            queue[1].counterType.should.equal('GAUGE')
+            queue[1].value.should.equal(1)
+
+            done()
+        })
+    })
 
     it('should tag metric', function(done) {
         var falcon = new Falcon({
             step: 0,
-        });
+        })
 
         var metric =  'metricName'
-        var value = 100;
+        var value = 100
         falcon
             .tag('key', 'value')
-            .gauge(metric, value);
+            .gauge(metric, value)
 
         getClientRequestBody(6060, function(queue) {
-            queue[0].tags.should.equal('project='+project+','+'key=value');
-            done();
-        });
-    });
+            queue[0].tags.should.equal('project='+project+','+'key=value')
+            done()
+        })
+    })
 
     it('should allow change step', function(done) {
         var falcon = new Falcon({
             step: 0,
-        });
+        })
 
         var metric =  'metricName'
-        var value = 100;
+        var value = 100
         falcon
             .step(1)
-            .gauge(metric, value);
+            .gauge(metric, value)
 
         getClientRequestBody(6060, function(queue) {
-            queue[0].step.should.equal(1);
-            done();
-        });
-    });
-});
+            queue[0].step.should.equal(1)
+            done()
+        })
+    })
+})
 
-var http = require('http');
+var http = require('http')
 function getClientRequestBody(port, callback) {
     var server = http.createServer(function(req, res) {
         if(req.method === "POST"){
-            var data = '';
+            var data = ''
 
             req.on('data', function(chunk){
-                data += chunk;
-            });
+                data += chunk
+            })
             req.on('end', function(){
-                end();
-                callback(JSON.parse(data));
-            });
+                end()
+                callback(JSON.parse(data))
+            })
         } else {
-            end();
+            end()
         }
 
         function end() {
-            res.end();
-            server.close();
+            res.end()
+            server.close()
         }
-    });
-    server.listen(port);
+    })
+    server.listen(port)
 }
